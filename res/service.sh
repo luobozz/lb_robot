9e28ca765b3cc350eef867ab6b63d5816ec739bc
luobozz <437337961@qq.com>
2021-10-22 16:33:31 +0800

    add service loader

--- res/service.sh
+++ res/service.sh
@@ -1,2 +1,6 @@
 #! /bin/bash
-# 本脚用来做service的手动更新来源是github仓库
+# 本脚本用来做service的手动更新来源是github仓库
+# 因为某原因依赖lgh命令代理 https://github.com/luobozz/my_shell/blob/main/mini_script/lgh
+
+lgh wget https://raw.githubusercontent.com/luobozz/lb_robot/master/res/service.tar
+tar -xvf service.tar
