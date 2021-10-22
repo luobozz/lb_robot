d3024cdb04948722547fbdb6e4dd0bf34b0beebd
luobozz <437337961@qq.com>
2021-10-22 16:49:31 +0800

    change service loader

--- res/service.sh
+++ res/service.sh
@@ -6,5 +6,5 @@ current=`date "+%Y-%m-%d %H:%M:%S"`
 timeStamp=`date -d "$current" +%s`   
 currentTimeStamp=$((timeStamp*1000+`date "+%N"`/1000000)) 
 
-lgh wget https://raw.githubusercontent.com/luobozz/lb_robot/master/res/service.tar?v=$currentTimeStamp
+lgh wget https://raw.githubusercontent.com/luobozz/lb_robot/master/res/service.tar?v=$currentTimeStamp -O service.tar
 tar -xvf service.tar
